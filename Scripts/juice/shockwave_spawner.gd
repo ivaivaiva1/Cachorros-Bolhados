cafb3c2aaf8d8f451433dc5268c6ee683eb01009
ivaivaiva1 <120752841+ivaivaiva1@users.noreply.github.com>
2025-11-11 01:11:21 -0300

    update: update shockwave

--- Scripts/juice/shockwave_spawner.gd
+++ Scripts/juice/shockwave_spawner.gd
@@ -10,7 +10,7 @@ func do_shockwave(pos: Vector2):
 	var shockwave_instance = shockwave_scene.instantiate()
 	add_child(shockwave_instance)
 	
-	const pos_offset = Vector2(220.0, 220.0)
+	const pos_offset = Vector2(3000.0, 3000.0)
 	shockwave_instance.global_position = pos - pos_offset
 	
 	# Duplicar o material do ColorRect
@@ -23,31 +23,43 @@ func do_shockwave(pos: Vector2):
 	var new_mat := mat.duplicate() as ShaderMaterial
 	shockwave_instance.material = new_mat
 	
-	
+	# Define os parâmetros iniciais do shader
 	new_mat.set_shader_parameter("center", Vector2(0.5, 0.5))
-	new_mat.set_shader_parameter("force", 0.1)
+	new_mat.set_shader_parameter("force", 1.0)
 	new_mat.set_shader_parameter("size", 0.0)
-	new_mat.set_shader_parameter("thickness", 0.25)
+	new_mat.set_shader_parameter("thickness", 0.1)
 	
-	
-		# Cria o tween de animação
+	# Cria o tween de animação
 	var tween := get_tree().create_tween()
 	tween.set_ease(Tween.EASE_OUT)
 	tween.set_trans(Tween.TRANS_CUBIC)
 	
-	# Anima o parâmetro "size" do shader de 0.0 → 0.9 em 0.6s
-	tween.tween_method(
+	# Os dois tweens principais começam ao mesmo tempo
+	# Crescimento do raio (0 → 1)
+	tween.parallel().tween_method(
 		func(value):
 			if is_instance_valid(new_mat):
 				new_mat.set_shader_parameter("size", value)
-	, 0.0, 0.9, 0.6)
+	, 0.0, 0.6, 0.3)
 	
-	# Depois de crescer, espera um pouco e faz fade-out do ColorRect (opcional)
-	tween.tween_interval(0.1)
-	tween.tween_property(shockwave_instance, "modulate:a", 0.0, 0.3)
+	# Decaimento da força (1 → 0)
+	tween.parallel().tween_method(
+		func(value):
+			if is_instance_valid(new_mat):
+				new_mat.set_shader_parameter("force", value)
+	, 1.0, 0, 0.3)
+	
+	tween.parallel().tween_method(
+		func(value):
+			if is_instance_valid(new_mat):
+				new_mat.set_shader_parameter("thickness", value)
+	, 0.1, 0.01, 0.3)
+	
+	## Depois de crescer e dissipar, faz o fade-out do ColorRect
+	#tween.tween_interval(0.1)
+	#tween.tween_property(shockwave_instance, "modulate:a", 0.0, 0.3)
 	
 	# Quando o tween terminar, remove a instância
 	tween.finished.connect(func():
 		if is_instance_valid(shockwave_instance):
-			shockwave_instance.queue_free()
-	)
+			shockwave_instance.queue_free())
